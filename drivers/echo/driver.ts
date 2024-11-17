0cd9d2690690078a30b8dda18f650aec77f5b3ad
Alexander Dimitrov <alexander.dimitrov@slr-holiday-service.bg>
2024-11-17 14:53:58 +0200

    fix: onPairListDevices usage

--- drivers/echo/driver.ts
+++ drivers/echo/driver.ts
@@ -39,15 +39,21 @@ module.exports = class EchoDriver extends Homey.Driver {
       .registerRunListener((args, _state) => this.api.executeRoutine(args.device.id, args.routine.id));
   }
 
+  async onPair(session: Homey.Driver.PairSession): Promise<void> {
+    if (!this.api.connected) {
+      throw new Error('Please check the connection to your Amazon account in the app settings');
+    }
+
+    session.setHandler("list_devices", async () => {
+      return await this.onPairListDevices();
+    });
+  }
+
   /**
    * onPairListDevices is called when a user is adding a device and the 'list_devices' view is called.
    * This should return an array with the data of devices that are available for pairing.
    */
   async onPairListDevices() {
-    if (!this.api.connected) {
-      throw new Error('Please check the connection to your Amazon account in the app settings');
-    }
-
     const devices = await this.api.getDevices();
 
     return devices
